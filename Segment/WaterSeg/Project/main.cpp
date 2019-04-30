d0fa1b80bd266b22c45e3deae40c236c0c389381
DJdongbudong <34826528+DJdongbudong@users.noreply.github.com>
2019-04-30 13:10:43 +0800

    Create main.cpp

---
+++ Segment/WaterSeg/Project/main.cpp
@@ -0,0 +1,17 @@
+#include "Segment_WaterSeg.h"
+int main()
+{
+	
+	Mat Mat_Image = imread("./cizhuan/A.bmp");//模板
+	Mat Mat_dst, Mat_dst_bw;
+	Segment_WaterSeg demo;
+	demo.Segment_WaterSegment(Mat_Image, Mat_dst, Mat_dst_bw);
+	namedWindow("着色结果", 0);
+	imshow("着色结果", Mat_dst);
+	namedWindow("二值结果", 0);
+	imshow("二值结果", Mat_dst_bw);
+	waitKey(0);
+
+	system("pasue");
+	return 0;
+}
