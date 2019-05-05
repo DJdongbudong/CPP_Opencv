84febfbeda64edd0f2c009e70e2ad22c370ea9bd
DJdongbudong <34826528+DJdongbudong@users.noreply.github.com>
2019-05-05 11:58:18 +0800

    Update Opencv_My_Segment_demo.cpp

--- Segment/WaterSeg/Opencv_My_Segment_demo.cpp
+++ Segment/WaterSeg/Opencv_My_Segment_demo.cpp
@@ -1,14 +1,12 @@
-/*
-	质心 + NO_ROI两个的坐标进行分水分割
-	#include <opencv2\opencv.hpp>
-	#include <opencv2\core\core.hpp>
-	#include <opencv2\highgui\highgui.hpp>
-	#include <opencv2\imgproc\imgproc.hpp>
-	#include <iostream>
+//质心 + NO_ROI两个的坐标进行分水分割
+#include <opencv2\opencv.hpp>
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
 
-	using namespace cv;
-	using namespace std;
-*/
 void Segment_WaterSegment(Mat Mat_Image, Mat& Mat_dst, Mat& Mat_dst_bw, Point No_ROI = Point(0, 0))
 {
 	// 目标质心寻找，仅使用二值化 和 非重心寻找 Point
@@ -18,16 +16,16 @@ void Segment_WaterSegment(Mat Mat_Image, Mat& Mat_dst, Mat& Mat_dst_bw, Point No
 	Mat bwImage;
 
 	threshold(grayImage, bwImage, 50, 255, THRESH_BINARY | THRESH_OTSU);
-	
+
 	morphologyEx(bwImage, bwImage, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(9, 9), Point(-1, -1)));
-	
+
 	Mat Labels;
 	distanceTransform(bwImage, bwImage, Labels, CV_DIST_L2, CV_DIST_MASK_3, 5);
 	normalize(bwImage, bwImage, 0, 1, NORM_MINMAX);
 	bwImage.convertTo(grayImage, CV_8UC1);
 	threshold(grayImage, bwImage, 0, 255, THRESH_BINARY | THRESH_OTSU);
 	morphologyEx(bwImage, grayImage, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(9, 9), Point(-1, -1)));
-	
+
 	namedWindow("grayImage", 0);
 	imshow("grayImage", grayImage);
 	waitKey(0);
@@ -59,7 +57,7 @@ void Segment_WaterSegment(Mat Mat_Image, Mat& Mat_dst, Mat& Mat_dst_bw, Point No
 		cout << contours_mask.size() << "个轮廓" << ";	" << "轮廓ROI定位失败！" << endl;
 		return;
 	}
-	
+
 	cout << contours_mask.size() << "个轮廓" << ";	" << "轮廓定位成功！" << endl;
 	vector<Moments> mu(contours_mask.size());
 	vector<Point2f> mc(contours_mask.size());
@@ -68,15 +66,15 @@ void Segment_WaterSegment(Mat Mat_Image, Mat& Mat_dst, Mat& Mat_dst_bw, Point No
 	}
 	for (int i = 0; i< contours_mask.size(); i++){
 		mc[i] = Point2d(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
-		cout << mc[i].x << mc[i].y << endl;		
-	}	
-	
-	/*ROI + NO_ROI*/	
+		cout << mc[i].x << mc[i].y << endl;
+	}
+
+	/*ROI + NO_ROI*/
 	Mat Mask_ROI_Image = Mat(Mat_Image.size(), CV_8UC1);
 	// No_ROI点 
-	circle(Mask_ROI_Image, No_ROI, 5, Scalar::all(255), 50, 40, 0);	
+	circle(Mask_ROI_Image, No_ROI, 5, Scalar::all(255), 50, 40, 0);
 	// ROI点
-	circle(Mask_ROI_Image, Point(mc[0].x, mc[0].y), 5, Scalar::all(255), 50, 40, 0);	
+	circle(Mask_ROI_Image, Point(mc[0].x, mc[0].y), 5, Scalar::all(255), 50, 40, 0);
 
 	findContours(Mask_ROI_Image, contours_mask, hierarchy_mask, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
 	if (contours_mask.size() != 2)
@@ -97,26 +95,26 @@ void Segment_WaterSegment(Mat Mat_Image, Mat& Mat_dst, Mat& Mat_dst_bw, Point No
 		rectangle(grayImage, boundRect, Scalar(0, 0, 255), 30);
 
 		drawContours(MaskWaterShed, contours_mask, index, Scalar::all(index + 1), -1, 8, hierarchy_mask, INT_MAX);
-	}	
+	}
 
 	// 分水岭 	size()一样
 	watershed(Mat_Image, MaskWaterShed);  // 
-	
+
 	// 区域着色
 	Mat resImage = Mat(MaskWaterShed.size(), CV_8UC3);  // 声明一个最后要显示的图像
 	Mat bw_resImage = Mat(MaskWaterShed.size(), CV_8UC1);  // 声明一个最后要显示的图像
 	for (int i = 0; i < MaskWaterShed.rows; i++)
 	{
 		for (int j = 0; j < MaskWaterShed.cols; j++)
-		{  
-			int index = MaskWaterShed.at<int>(i, j);  
-			if (index == MaskWaterShed.at<int>(mc[0].y ,mc[0].x))	//ROI一类
+		{
+			int index = MaskWaterShed.at<int>(i, j);
+			if (index == MaskWaterShed.at<int>(mc[0].y, mc[0].x))	//ROI一类
 			{
 				resImage.at<Vec3b>(i, j) = Mat_Image.at<Vec3b>(i, j);
 				bw_resImage.at<uchar>(i, j) = 255;
-			}		
+			}
 			else
-			{				
+			{
 				resImage.at<Vec3b>(i, j) = Vec3b(0, 0, 0);	//非ROI一类
 				bw_resImage.at<uchar>(i, j) = 0;
 			}
@@ -126,13 +124,13 @@ void Segment_WaterSegment(Mat Mat_Image, Mat& Mat_dst, Mat& Mat_dst_bw, Point No
 	//Mat_dst_bw = Mat(MaskWaterShed.size(), CV_8UC1);  // 声明一个最后要显示的图像
 	Mat_dst = resImage.clone();
 	Mat_dst_bw = bw_resImage.clone();
-	
+
 }
 
 
 int main()
 {
-	
+
 	Mat Mat_Image = imread("./cizhuan/A.bmp");//模板
 	Mat Mat_dst, Mat_dst_bw;
 	Segment_WaterSegment(Mat_Image, Mat_dst, Mat_dst_bw);
